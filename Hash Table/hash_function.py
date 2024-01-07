21aef4eb1ba47f7abcb6cf866f2c5f87baac2c7c
athishulleri01 <athishulleri0000@gmail.com>
2024-01-07 20:29:28 +0530

    Hash Table

---
+++ Hash Table/hash_function.py
@@ -0,0 +1,27 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+        
+        
+    def get_hash(self, key):
+        h = 0
+        for i in key:
+            h += ord(i)
+        return h % self.MAX
+    def __setitem__(self,key,value):
+        h = self.get_hash(key)
+        self.arr[h]=value
+        # return self.arr
+    
+    def __getitem__(self,key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+        
+h = HashTable()
+h.add('march',3)
+result = h.get('march')
+print(result)
+
+        
