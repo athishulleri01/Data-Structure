a6c8b99b6a5bbf074b7adf0196833db8c48ffbc0
athishulleri01 <athishulleri0000@gmail.com>
2024-01-14 23:47:20 +0530

    Tree

---
+++ demo.py
@@ -0,0 +1,17 @@
+# def factorial(num):
+#     if num == 0:
+#         return 1
+#     return num * factorial(num-1)
+
+# result = factorial(4)
+# print(result)
+
+# list1 = ["apple","orange","graphe"]
+# list2 = ["banana","apple","lemon"]
+# list1 = list1+list2
+# list2 = list(set(list1))
+# print(list2)
+
+# l1 = [1,2,3,4,5,6]
+# l2 = [if x%2==0 for x in l1 else x ]
+# print(l2)
