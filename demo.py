c4e4ed1d54b26feabb4ba5ad91e6c34ac7eaaa93
athishulleri01 <athishulleri0000@gmail.com>
2024-01-20 05:04:18 +0530

    BST

--- demo.py
+++ demo.py
@@ -15,3 +15,14 @@
 # l1 = [1,2,3,4,5,6]
 # l2 = [if x%2==0 for x in l1 else x ]
 # print(l2)
+
+
+even = [x if x % 2 == 0 else 0 for x in range(1, 11)]
+odd = [x if x % 2 != 0 else 0 for x in range(1, 11)]
+
+# Filter out the zeros
+even = [x for x in even if x != 0]
+odd = [x for x in odd if x != 0]
+
+print("Even numbers:", even)
+print("Odd numbers:", odd)
